87889ee257d848d0a162d91bb0ccb6eabfba6cbb
jake0102 <jakejapan0102@gmail.com>
2022-05-21 13:29:45 +0900

    new

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ function handleToggle(){
   }
 }
 
-;(()=>{
+;(() => {
   window.onscroll = function() {scrollFunction()};
 
   function scrollFunction() {
@@ -65,7 +65,7 @@ function handleToggle(){
   }
 })()
 
-;(()=>{
+;(() => {
   let x = document.querySelector('.banner-container-main')
 
   const handleScroll = function(){
@@ -87,7 +87,7 @@ function handleToggle(){
   handleScroll()
 })()
 
-;(()=>{
+;(() => {
   const menuTrigger = document.querySelector('.menu-trigger')
 
   const navPlus = document.querySelector('.header-nav-plus')
@@ -116,7 +116,7 @@ function handleToggle(){
   }
 })()
 
-;(()=>{
+;(() => {
   const live = document.querySelector('#live')
 
   const modal = document.querySelector('.myModal')
@@ -173,4 +173,66 @@ function handleToggle(){
 
     next.style.display = 'none'
   }
+})()
+
+;(() => {
+  const now = new Date();
+
+  const nowM = new Date()
+
+  const wd = ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日'];
+
+  const htmlDate = document.querySelectorAll('.calendar-container-main-head-item-top-span')
+
+  const htmlDay = document.querySelectorAll('.calendar-container-main-head-item-bot')
+
+  const htmlMonth = document.querySelectorAll('.calendar-container-main-head-item-top-m')
+
+  const htmlDateMobile = document.querySelectorAll('.mobile-date')
+
+  const htmlDayMobile = document.querySelectorAll('.mobile-day')
+
+  const htmlMonthMobile = document.querySelectorAll('.mobile-month')
+
+  htmlDate.forEach(element => {
+    let d = now.getDate()
+    let w = wd[now.getDay()]
+    now.setDate(now.getDate()+1)
+
+    element.innerHTML = d
+  })
+
+  htmlDay.forEach(element => {
+    let d = now.getDate()
+    let w = wd[now.getDay()]
+    now.setDate(now.getDate()+1)
+
+    element.innerHTML = w
+  })
+
+  htmlMonth.forEach(element => {
+    let m = now.getMonth()
+
+    element.innerHTML = m
+  })
+
+  htmlDateMobile.forEach(element => {
+    let currentDate = nowM.getDate()
+    nowM.setDate(nowM.getDate()+1)
+
+    element.innerHTML = currentDate
+  })
+
+  htmlDayMobile.forEach(element => {
+    let w = wd[now.getDay()]
+    now.setDate(now.getDate()+1)
+
+    element.innerHTML = w
+  })
+
+  htmlMonthMobile.forEach(element => {
+    let m = now.getMonth()
+
+    element.innerHTML = m
+  })
 })()
