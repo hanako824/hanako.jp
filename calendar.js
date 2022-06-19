3b30858b61c7d906d07f69e07155d36492d35e14
jake0102 <jakejapan0102@gmail.com>
2022-06-19 13:19:55 +0900

    new

--- calendar.js
+++ calendar.js
@@ -1,19 +1,80 @@
 // Nav Plus
 ;(() => {
-    const menuTrigger = document.querySelector('.menu-trigger')
+  const menuTrigger = document.querySelector('.menu-trigger')
+
+  const navPlus = document.querySelector('.header-nav-plus')
+
+  const navPlusM = document.querySelector('.header-nav-plus-mobile')
+
+  const headerNav = document.querySelector('.header-nav')
+
+  menuTrigger.onclick = () => {
+
+    menuTrigger.classList.toggle('active')
+
+    navPlus.classList.toggle('show')
+
+    navPlusM.classList.toggle('show')
+
+    if(navPlus.classList.length == 2){
+      headerNav.style.background = "rgb(70, 70, 70)";
+    }else{
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        headerNav.style.background = "rgb(70, 70, 70)";
+      } else {
+        headerNav.style.background = "";
+      }
+    }
+  }
+})()
+
+// Header toggle
+;(() => {
+  const toggleA = document.querySelectorAll('.toggleA')
+
+  toggleA.forEach(element => {
+    element.onclick = () => {
+      const menuTrigger = document.querySelector('.menu-trigger')
     
-    const navPlus = document.querySelector('.header-nav-plus')
+      const navPlus = document.querySelector('.header-nav-plus')
     
-    const navPlusM = document.querySelector('.header-nav-plus-mobile')
+      const navPlusM = document.querySelector('.header-nav-plus-mobile')
     
-    menuTrigger.onclick = () => {
-    
+      const headerNav = document.querySelector('.header-nav')
+      
       menuTrigger.classList.toggle('active')
     
       navPlus.classList.toggle('show')
     
       navPlusM.classList.toggle('show')
+    
+      if(navPlus.classList.length == 2){
+        headerNav.style.background = "rgb(70, 70, 70)";
+      }else{
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+          headerNav.style.background = "rgb(70, 70, 70)";
+        } else {
+          headerNav.style.background = "";
+        }
+      }
     }
+  })
+})()
+
+// Scroll Header
+;(() => {
+  window.onscroll = function() {scrollFunction()};
+
+  function scrollFunction() {
+
+    let headerNav = document.querySelector('.header-nav')
+
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      headerNav.style.background = "rgb(70, 70, 70)";
+    } else {
+      headerNav.style.background = "";
+    }
+  }
 })()
 
 // Calendar
